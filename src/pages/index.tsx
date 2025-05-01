9f7891ba88638cb8264b6ddec5cf3d232e41c54a
unknown <quocdoanfb@gmail.com>
2025-05-01 21:01:15 +0700

    check 02

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Button variant='contained'>Hello world check deploy </Button>
+      <Button variant='contained'>Hello world check deploy 1 </Button>
     </>
   )
 }
