04248751c6a98c22df6661776c65b762c8a68dc5
unknown <quocdoanfb@gmail.com>
2025-05-03 12:33:12 +0700

    UI-Login 50%

--- src/components/text-field/index.tsx
+++ src/components/text-field/index.tsx
@@ -26,10 +26,56 @@ const TextFieldStyle = styled(TextField)<TextFieldProps>(({ theme }) => {
             },
             ".MuiInputBase-input": {
                 padding: "8px 10px"
+            },
+            '&.Mui-error': {
+                borderColor: theme.palette.error.main
+            },
+            '&.Mui-focused': {
+                boxShadow: theme.shadows[2],
+                '& .MuiInputBase-input:not(.MuiInputBase-readOnly):not([readonly])::placeholder': {
+                    transform: 'translateX(4px)'
+                },
+                '&.MuiInputBase-colorPrimary': {
+                    borderColor: theme.palette.primary.main
+                },
+                '&.MuiInputBase-colorSecondary': {
+                    borderColor: theme.palette.secondary.main
+                },
+                '&.MuiInputBase-colorInfo': {
+                    borderColor: theme.palette.info.main
+                },
+                '&.MuiInputBase-colorSuccess': {
+                    borderColor: theme.palette.success.main
+                },
+                '&.MuiInputBase-colorWarning': {
+                    borderColor: theme.palette.warning.main
+                },
+                '&.MuiInputBase-colorError': {
+                    borderColor: theme.palette.error.main
+                },
+                '&.Mui-error': {
+                    borderColor: theme.palette.error.main
+                }
+            },
+
+            '&.Mui-disabled': {
+                backgroundColor: `${theme.palette.action.selected} !important`
+            },
+            '& .MuiInputAdornment-root': {
+                marginTop: '0 !important'
             }
+
         },
 
-
+        '& .MuiFormHelperText-root': {
+            lineHeight: 1.154,
+            margin: theme.spacing(1, 0, 0),
+            color: theme.palette.text.secondary,
+            fontSize: theme.typography.body2.fontSize,
+            '&.Mui-error': {
+                color: theme.palette.error.main
+            }
+        }
 
     }
 })
