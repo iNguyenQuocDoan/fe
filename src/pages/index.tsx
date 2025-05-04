2320c80ed690226cf6b86a5e30e080e26b9273aa
unknown <quocdoanfb@gmail.com>
2025-05-04 22:46:35 +0700

    Build layout basic (dashboard)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,9 +28,9 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Box sx={{ margin: 6, width: "200px" }}>
+      {/* <Box sx={{ margin: 6, width: "200px" }}>
         <CustomTextField id='outlined-multiline-flexible' label='text' />
-      </Box>
+      </Box> */}
     </>
   )
 }
